80a50bd61ab770120f791042b3458365cd5b93ba
Sorskye <teun.maalderink@gmail.com>
2026-03-11 10:47:36 +0100

    Initial commit
    base project

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NEXT.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
