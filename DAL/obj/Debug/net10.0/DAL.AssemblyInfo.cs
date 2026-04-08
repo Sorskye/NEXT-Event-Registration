5fbaa3f8ccf40ed7dff2820d877b19ea7b0b490c
Somar <563611@student.fontys.nl>
2026-04-08 13:32:34 +0200

    Initial commit

--- DAL/obj/Debug/net10.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net10.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4e6c08f776623d6097a275f702ef6e7c6cff14")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
