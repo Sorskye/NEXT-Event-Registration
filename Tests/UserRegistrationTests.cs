b0e9a10103989313bf52faa7f427bbb9c2b8739f
Seldrinho <jaydenselder2@gmail.com>
2026-05-21 13:07:56 +0200

    Eerste unit tests
    ik heb het registreren voor events getest

---
+++ Tests/UserRegistrationTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NEXT.Pages.Dashboards;
+using NEXT.Tests;
+using NEXT.Tests.Fakes;
+using Xunit;
+
+namespace Tests
+{
+    public class UserRegistrationTests
+    {
+        [Fact]
+        public void OnPostRegisterUser_WithoutSession_ReturnsRedirectToLogin()
+        {
+            // Arrange
+            var fakeUserRepo = new FakeUserRepository();
+            var fakeEventRegRepo = new FakeEventRegistrationRepository();
+            var httpContext = new DefaultHttpContext();
+            httpContext.Session = new TestSession();
+            var pageModel = new UserDashboardModel(fakeEventRegRepo, fakeUserRepo)
+            {
+                PageContext = new PageContext
+                {
+                    HttpContext = httpContext
+                }
+            };
+
+            // Act
+            IActionResult result = pageModel.OnPostRegister(1);
+
+            // Assert
+            Assert.IsType<RedirectToPageResult>(result);
+            var redirectResult = result as RedirectToPageResult;
+            Assert.Equal("/Auth/Login", redirectResult.PageName);
+        }
+
+        [Fact]
+        public void OnPostRegisterUser_WithSession_RegistersUserForEventAndRedirects()
+        {
+            // Arrange
+            var fakeUserRepo = new FakeUserRepository();
+            var fakeEventRegRepo = new FakeEventRegistrationRepository();
+            var pageModel = new UserDashboardModel(fakeEventRegRepo, fakeUserRepo);
+
+            var session = new TestSession();
+            session.SetInt32("UserId", 42);
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Session = session;
+
+            pageModel.PageContext = new PageContext
+            {
+                HttpContext = httpContext
+            };
+
+            int eventId = 1;
+
+            // Act
+            IActionResult result = pageModel.OnPostRegister(eventId);
+
+            // Assert
+            Assert.IsType<RedirectToPageResult>(result);
+            Assert.True(fakeEventRegRepo.RegisterUserForEventCalled);
+            Assert.Equal(42, fakeEventRegRepo.RegisteredUserId);
+            Assert.Equal(1, fakeEventRegRepo.RegisteredEventId);
+        }
+    }
+}
